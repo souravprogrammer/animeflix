14b8953a39ddb71e730ff53834e819cb8bc8cc67
sourav sharma <souravsh1234567@gmail.com>
2023-05-13 13:02:00 +0530

    added PWA

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa")({
+  // reactStrictMode: true,
+  dest: "public",
+  skipWaiting: true,
+  // disable: process.env.NODE_ENV === "development",
+});
 const nextConfig = {
   reactStrictMode: true,
 };
 
-module.exports = nextConfig;
+module.exports = withPWA({
+  reactStrictMode: true,
+});
+
+// /nextConfig;
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,7 +5,14 @@ import { useSession } from "next-auth/react";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
